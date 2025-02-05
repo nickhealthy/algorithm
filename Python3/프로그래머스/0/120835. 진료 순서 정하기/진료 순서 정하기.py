8d378c76219fca427e492cf87c1d643e38f5f5ab
nick <alskadmlcraz1@gmail.com>
2025-02-05 14:13:17 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ Python3/프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -12,7 +12,16 @@ def solution(emergency):
     """
     
     """두 번째 풀이 - 파이썬닉하게 풀이
-    """    
     desending_list = sorted(emergency, reverse=True)
     return [desending_list.index(e) + 1 for e in emergency]
+    """    
+
+    """세 번째 풀이 - dict 사용"""
+    desending_dict = {val: idx + 1 for idx, val in enumerate(sorted(emergency)[::-1])}
+    
+    answer = []
+    for value in emergency:
+        answer.append(desending_dict[value])
+    
+    return answer
     
