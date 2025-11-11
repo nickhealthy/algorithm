0c5a41ad49371b702c0931e1a3053de72f76812d
nick <alskadmlcraz1@gmail.com>
2025-11-11 20:21:59 +0900

    [level 1] Title: 덧칠하기, Time: 1.49 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
+        /**
         int answer = 0;
         int pos = section[0];
         
@@ -7,6 +8,18 @@ class Solution {
             if (pos <= sec) {
                 pos = m + sec;
                 answer++;
+            }
+        }
+        
+        return answer;
+        */
+        int answer = 1;
+        int pos = section[0];
+        
+        for (int i = 0; i < section.length; i++) {
+            if (pos + m - 1 < section[i]) {
+                answer++;
+                pos = section[i];
             }
         }
         
