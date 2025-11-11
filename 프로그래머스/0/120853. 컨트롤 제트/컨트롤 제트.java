8247a62b673bec1e104e17e25d8b58a77ef1ea09
nick <alskadmlcraz1@gmail.com>
2025-11-11 17:09:58 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.20 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] strArr = s.split(" ");
+        for (int i = 0; i < strArr.length; i++) {
+            if (strArr[i].equals("Z")) {
+                answer -= Integer.valueOf(strArr[i - 1]);
+                continue;
+            }
+            
+            answer += Integer.valueOf(strArr[i]);
+        }
+        return answer;
+    }
+}
