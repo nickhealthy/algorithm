a76a68f4b31d1546e0ed2f9541e4fc806f664fbe
nick <alskadmlcraz1@gmail.com>
2025-12-15 20:48:01 +0900

    [Silver II] Title: 스택 수열, Time: 276 ms, Memory: 27264 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,31 +2,34 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int temp = 1;
-    static boolean isNotValid = false;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
         
         int N = Integer.valueOf(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int num = Integer.valueOf(br.readLine());
-            
-            for (; temp <= num; temp++) {
-                stack.add(temp);
-                sb.append("+\n");
+        int[] arr = new int[N];
+        int start = 0;
+        int idx = 0;
+        
+        while (N-- > 0) {
+            int value = Integer.valueOf(br.readLine());
+            if (start < value) {
+                for (int i = start + 1; i <= value; i++) {
+                    arr[idx++] = i;
+                    sb.append("+").append("\n");
+                }
+                start = value;
             }
             
-            if (stack.peek() == num) {
-                stack.pop();
-                sb.append("-\n");
-            } else isNotValid = true;
+            else if (arr[idx - 1] != value) {
+                System.out.println("NO");
+                return;
+            }
+            
+            idx--;
+            sb.append("-").append("\n");
         }
         
-        if (isNotValid) {
-            System.out.println("NO");
-        } else System.out.println(sb);
+        System.out.println(sb);
     }
 }
