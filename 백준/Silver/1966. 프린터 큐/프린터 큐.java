3f6d5143372f1e4791ceb197e5ea05d859b3d555
nick <alskadmlcraz1@gmail.com>
2025-12-16 15:34:40 +0900

    [Silver III] Title: 프린터 큐, Time: 136 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -9,43 +9,48 @@ class Main {
         int T = Integer.valueOf(br.readLine());
         while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.valueOf(st.nextToken());
-            int target = Integer.valueOf(st.nextToken());
             
+            int N = Integer.valueOf(st.nextToken());
+            int M = Integer.valueOf(st.nextToken());
+            
+            Deque<int[]> q = new ArrayDeque<>();
             st = new StringTokenizer(br.readLine());
-            LinkedList<int[]> q = new LinkedList<>();
             for (int i = 0; i < N; i++) {
-                // 순서, 중요도
-                q.offer(new int[] {i, Integer.valueOf(st.nextToken())});
+                q.offerLast(new int[] {i, Integer.valueOf(st.nextToken())}); // 순번, 중요도
             }
             
             int count = 0;
             while (!q.isEmpty()) {
-                int[] front = q.poll(); // 첫 번째 원소
-                boolean isMax = true; // 현재 원소 중요도 체크
+                boolean isMax = true;
+                int[] front = q.pollFirst();
                 
-                for (int i = 0; i < q.size(); i++) {
-                    // 현재 프린터 중요도 보다 뒤에 원소가 크다면, 현재 원소 + i 전까지 원소 뒤로 보내기
-                    if (front[1] < q.get(i)[1]) {
-                        q.offer(front);
-                        for (int j = 0; j < i; j++) {
-                            q.offer(q.poll());
+                Iterator<int[]> iter = q.iterator();
+                int moveCount = 0;
+                while (iter.hasNext()) {
+                    int[] next = iter.next();
+                    if (front[1] < next[1]) {
+                        q.offerLast(front);
+                        for (int j = 0; j < moveCount; j++) {
+                            q.offerLast(q.pollFirst());
                         }
                         isMax = false;
                         break;
                     }
+                    moveCount++;
                 }
                 
                 if (!isMax) continue;
                 
-                // 현재 원소(front)가 제일 크다면 출력해야 하는 문서
+                // 여기까지 오면 출력해야 하는 문서
                 count++;
-                if (front[0] == target) { // 해당 테스트 케이스에서 원하는 문서 찾았으므로 break
+                // 찾고 있는 문서가 현재 출력물이라면
+                if (front[0] == M) {
                     sb.append(count).append("\n");
                     break;
-                }   
+                }
             }
         }
+        
         System.out.println(sb);
         br.close();
     }
