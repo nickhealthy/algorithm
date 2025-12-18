565d2abe16658a4e00aef5d2e0e7bd5b7e270538
nick <alskadmlcraz1@gmail.com>
2025-12-18 14:42:51 +0900

    [Silver III] Title: 프린터 큐, Time: 132 ms, Memory: 14948 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -13,10 +13,10 @@ class Main {
             int N = Integer.valueOf(st.nextToken());
             int M = Integer.valueOf(st.nextToken());
             
-            Deque<int[]> q = new ArrayDeque<>();
             st = new StringTokenizer(br.readLine());
+            Deque<int[]> q = new ArrayDeque<>();
             for (int i = 0; i < N; i++) {
-                q.offerLast(new int[] {i, Integer.valueOf(st.nextToken())}); // 순번, 중요도
+                q.offerLast(new int[] {i, Integer.valueOf(st.nextToken())});
             }
             
             int count = 0;
@@ -24,30 +24,27 @@ class Main {
                 boolean isMax = true;
                 int[] front = q.pollFirst();
                 
+                // 중요도 확인 및 front가 1순위가 아니라면 다시 뒤로 배치
                 Iterator<int[]> iter = q.iterator();
-                int moveCount = 0;
+                int temp = 0;
                 while (iter.hasNext()) {
                     int[] next = iter.next();
                     if (front[1] < next[1]) {
                         q.offerLast(front);
-                        for (int j = 0; j < moveCount; j++) {
+                        for (int i = 0; i < temp; i++) {
                             q.offerLast(q.pollFirst());
                         }
                         isMax = false;
                         break;
                     }
-                    moveCount++;
+                    temp++;
                 }
                 
                 if (!isMax) continue;
-                
-                // 여기까지 오면 출력해야 하는 문서
+
+                // 출력
                 count++;
-                // 찾고 있는 문서가 현재 출력물이라면
-                if (front[0] == M) {
-                    sb.append(count).append("\n");
-                    break;
-                }
+                if (front[0] == M) sb.append(count).append("\n");
             }
         }
         
