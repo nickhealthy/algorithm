fbbf97dd601bdb837625ed452f06c04dce08acf1
nick <alskadmlcraz1@gmail.com>
2026-03-26 15:01:51 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -2,15 +2,13 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    // 북, 동, 남, 서
     static final int[][] DIRECTIONS = {
         {-1, 0}, {0, 1}, {1, 0}, {0, -1}
     };
     
-    static int N, M;
-    static boolean[][] visited;
+    static int N, M, startR, startC, startD;
     static int[][] graph;
-    static int startR, startC, startD;
+    static boolean[][] visited;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,7 +21,7 @@ class Main {
         startR = Integer.parseInt(st.nextToken());
         startC = Integer.parseInt(st.nextToken());
         startD = Integer.parseInt(st.nextToken());
-
+        
         graph = new int[N][M];
         visited = new boolean[N][M];
         for (int row = 0; row < N; row++) {
@@ -35,33 +33,34 @@ class Main {
         }
         
         System.out.println(simulate(startR, startC, startD));
+        
     }
     
-    static int simulate(int r, int c, int d) {
+    private static int simulate(int r, int c, int d) {
         int result = 0;
+        
         while (true) {
             if (!visited[r][c]) {
                 result++;
                 visited[r][c] = true;
             }
-        
+            
             if (hasUncleanedAround(r, c)) {
-                d = (d + 3) % 4; // 반시계 회전
+                d = (d + 3) % 4;
                 int nr = r + DIRECTIONS[d][0];
                 int nc = c + DIRECTIONS[d][1];
-            
+                
                 if (isInBounds(nr, nc) && !visited[nr][nc]) {
                     r = nr;
                     c = nc;
                 }
+                
             } else {
-                int backD = (d + 2) % 4; // 후진
+                int backD = (d + 2) % 4;
                 int nr = r + DIRECTIONS[backD][0];
                 int nc = c + DIRECTIONS[backD][1];
                 
-                if (!isInBounds(nr, nc) || graph[nr][nc] == 1) {
-                    break;
-                }
+                if (!isInBounds(nr, nc) || graph[nr][nc] == 1) break;
                 
                 r = nr;
                 c = nc;
@@ -71,8 +70,8 @@ class Main {
         return result;
     }
     
-    static boolean hasUncleanedAround(int r, int c) {
-        for (int[] dir : DIRECTIONS) {
+    private static boolean hasUncleanedAround(int r, int c) {
+        for(int[] dir: DIRECTIONS) {
             int nr = r + dir[0];
             int nc = c + dir[1];
             
@@ -82,8 +81,7 @@ class Main {
         return false;
     }
     
-    static boolean isInBounds(int nx, int ny) {
-        return nx >= 0 && nx < N && ny >= 0 && ny < M;
+    private static boolean isInBounds(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < M;
     }
-    
 }
