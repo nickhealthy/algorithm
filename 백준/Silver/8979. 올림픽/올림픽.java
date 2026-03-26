9d4c26a610713cc077c2b39c27ee14122bc885b9
nick <alskadmlcraz1@gmail.com>
2026-03-26 19:05:31 +0900

    [Silver V] Title: 올림픽, Time: 104 ms, Memory: 13976 KB, Score: 8 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -9,31 +9,28 @@ class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         
-        int[][] arr = new int[n][4];
+        int[][] arr = new int[n + 1][3];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][2] = Integer.parseInt(st.nextToken());
-            arr[i][3] = Integer.parseInt(st.nextToken());
+            int country = Integer.parseInt(st.nextToken());
+            arr[country][0] = Integer.parseInt(st.nextToken());
+            arr[country][1] = Integer.parseInt(st.nextToken());
+            arr[country][2] = Integer.parseInt(st.nextToken());
         }
-        
-        Arrays.sort(arr, (a, b) -> {
-           if (b[1] != a[1]) return b[1] - a[1];
-           else if (b[2] != a[2]) return b[2] - a[2];
-           else return b[3] - a[3];
-        });
         
         int rank = 1;
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && (arr[i][1] != arr[i - 1][1] || arr[i][2] != arr[i - 1][2] || arr[i][3] != arr[i - 1][3]))
-                rank = i + 1;
-            
-            if (arr[i][0] == k) {
-                System.out.println(rank);
-                return;
-            }
+        for (int i = 1; i < n; i++) {
+            if (i == k) continue;
+            if (isBetter(arr[i], arr[k])) rank++;
         }
         
+        System.out.println(rank);
+        
+    }
+    
+    private static boolean isBetter(int[] a, int[] b) {
+        if (a[0] != b[0]) return a[0] > b[0];
+        if (a[1] != b[1]) return a[1] > b[1];
+        return a[2] > b[2];
     }
 }
