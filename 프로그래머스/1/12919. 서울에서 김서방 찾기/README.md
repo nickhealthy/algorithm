a3020d03044227467f3c2bd636511a4031379e47
nick <alskadmlcraz1@gmail.com>
2026-09-08 17:01:57 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
 
 ### 성능 요약
 
-메모리: 4.82 MB, 시간: 0.01 ms
+메모리: 4.82 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 08일 16:57:54
+2026년 09월 08일 17:01:54
 
 ### 문제 설명
 
--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 string solution(vector<string> seoul) {
-    auto it = find(seoul.begin(), seoul.end(), "Kim");
-    int idx = distance(seoul.begin(), it);
+//     auto it = find(seoul.begin(), seoul.end(), "Kim");
+//     int idx = distance(seoul.begin(), it);
     
-    return "김서방은 " + to_string(idx) + "에 있다";
+//     return "김서방은 " + to_string(idx) + "에 있다";
+    
+    for (auto it = seoul.begin(); it != seoul.end(); it++) {
+        if (*it == "Kim") {
+            return "김서방은 " + to_string(it - seoul.begin()) + "에 있다";
+        }
+    }
 }
