61b55bd2eb10166702c6a6e994462564c460d612
nick <alskadmlcraz1@gmail.com>
2026-09-10 01:11:07 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for (auto command: commands) {
+        int i = command[0];
+        int j = command[1];
+        int k = command[2];
+        
+        vector<int> temp;
+        for (int idx = i - 1; idx < j; idx++) {
+            temp.push_back(array[idx]);
+        }
+        
+        sort(temp.begin(), temp.end());
+        
+        answer.push_back(temp[k - 1]);
+    }
+
+    return answer;
+}
