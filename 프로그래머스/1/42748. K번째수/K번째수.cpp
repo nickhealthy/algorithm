706463582c41373ace16a441381c285de695503b
nick <alskadmlcraz1@gmail.com>
2026-09-10 14:48:55 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,13 +1,38 @@
-#include <string>
-#include <vector>
-#include <bits/stdc++.h>
+// #include <string>
+// #include <vector>
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+//     vector<int> answer;
+    
+//     for (auto command: commands) {
+//         int i = command[0];
+//         int j = command[1];
+//         int k = command[2];
+        
+//         vector<int> temp;
+//         for (int idx = i - 1; idx < j; idx++) {
+//             temp.push_back(array[idx]);
+//         }
+        
+//         sort(temp.begin(), temp.end());
+        
+//         answer.push_back(temp[k - 1]);
+//     }
+
+//     return answer;
+// }
+
+#include<bits/stdc++.h>
 
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
-    for (auto command: commands) {
+    for (vector<int> command: commands) {
         int i = command[0];
         int j = command[1];
         int k = command[2];
@@ -18,9 +43,9 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
         }
         
         sort(temp.begin(), temp.end());
-        
         answer.push_back(temp[k - 1]);
     }
-
+    
+    
     return answer;
 }
