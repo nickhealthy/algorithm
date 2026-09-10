402eda6199f826d5a18034391a68d2f6bf98057d
nick <alskadmlcraz1@gmail.com>
2026-09-10 20:03:24 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -6,17 +6,20 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int n = citations.size();
-
+    
+    // 논문 총 개수 n개 보다 h가 클 수 없음
     for (int h = n; h >= 0; h--) {
         int count = 0;
-
-        for (int citation : citations) {
-            if (citation >= h)
+        
+        for (int citation: citations) {
+            if (citation >= h) {
                 count++;
+                
+                if (count >= h) {
+                    return h;
+                }
+            }
         }
-
-        if (count >= h)
-            return h;
     }
 
     return 0;
