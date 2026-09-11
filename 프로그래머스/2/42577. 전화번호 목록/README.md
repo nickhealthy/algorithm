907496928023bd2faca93dad52f6ae8cb0356e5d
nick <alskadmlcraz1@gmail.com>
2026-09-11 19:08:05 +0900

    [level 2] Title: 전화번호 목록, Time: 118.83 ms, Memory: 48.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 35.8 MB, 시간: 43.65 ms
+메모리: 48.8 MB, 시간: 118.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 07일 23:49:02
+2026년 09월 11일 19:08:01
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,13 +1,87 @@
+// /*
+// // 1번쨰 풀이 - sort
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// bool solution(vector<string> phone_book) {
+//     sort(phone_book.begin(), phone_book.end());
+
+//     for (int i = 0; i < phone_book.size() - 1; i++) {
+//         if (phone_book[i + 1].find(phone_book[i]) == 0) {
+//             return false;
+//         }
+//     }
+    
+//     return true;
+// }
+// */
+
+
+// // 2번째 풀이 - prefix
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// bool solution(vector<string> phone_book) {
+//     unordered_set<string> phone_numbers(
+//         phone_book.begin(),
+//         phone_book.end()
+//     );
+    
+//     for (string phone : phone_book) {
+//         string prefix;
+        
+//         for (char c : phone) {
+//             prefix += c;
+        
+//             if (prefix != phone && phone_numbers.contains(prefix)) {
+//                 return false;
+//             }
+//         }
+//     }    
+    
+//     return true;
+// }
+
+// /*
+// // 3번째 방식 - 완탐(시간초과)
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// bool solution(vector<string> phone_book) {
+//     const int PHONE_BOOK_SIZE = phone_book.size();
+    
+//     for (int i = 0; i < PHONE_BOOK_SIZE; i++) {
+//         for (int j = 0; j < PHONE_BOOK_SIZE; j++) {
+//             if (i == j) continue;
+            
+//             if (phone_book[j].find(phone_book[i]) == 0) {
+//                 return false;
+//             }
+//         }
+//     }
+    
+//     return true;
+// }
+// */
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    sort(phone_book.begin(), phone_book.end());
+    unordered_set<string> s(phone_book.begin(), phone_book.end());
 
-    for (int i = 0; i < phone_book.size() - 1; i++) {
-        if (phone_book[i + 1].find(phone_book[i]) == 0) {
-            return false;
+    for (const string& num: phone_book) {
+        string prefix = "";
+        for (char ch: num) {
+            prefix += ch;
+            
+            if (prefix != num && s.count(prefix)) {
+                return false;
+            }
         }
     }
     
