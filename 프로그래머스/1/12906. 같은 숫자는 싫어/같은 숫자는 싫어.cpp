ab1fd3d57a958ba8e397339754f495eb0d05a73c
nick <alskadmlcraz1@gmail.com>
2026-09-16 23:23:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 96.27 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -2,19 +2,34 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> arr) 
-{   
-    int write = 0;
-    int n = (int)arr.size();
-    for (int read = 1; read < n; read++) {
-        if (arr[write] != arr[read]) {
-            arr[++write] = arr[read];
+// 2026-09-16 투포인터
+// vector<int> solution(vector<int> arr) 
+// {   
+//     int write = 0;
+//     int n = (int)arr.size();
+//     for (int read = 1; read < n; read++) {
+//         if (arr[write] != arr[read]) {
+//             arr[++write] = arr[read];
+//         }
+//     }
+    
+//     arr.resize(write + 1);
+
+//     return arr;
+// }
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    
+    answer.push_back(arr[0]);
+    int n = (int) arr.size();
+    for (int i = 1; i < n; i++) {
+        if (answer.back() != arr[i]) {
+            answer.push_back(arr[i]);
         }
     }
     
-    arr.resize(write + 1);
-
-    return arr;
+    return answer;
 }
 
 // unordered_set<pair>로 풀어보기
