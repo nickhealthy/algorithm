02d3aa7c291c94750efe07ef45c62fce2bbad6d3
nick <alskadmlcraz1@gmail.com>
2026-09-21 21:46:14 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -36,26 +36,53 @@
 //     return answer;
 // }
 
+
+// 2026.09.21 - index 변수 포인터 사용
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// vector<int> solution(vector<int> progresses, vector<int> speeds) {
+//     vector<int> answer;
+//     int head = 0, n = (int) progresses.size();
+    
+//     while (head < n) {
+//         for (int i = head; i < n; i++) {
+//             progresses[i] += speeds[i];
+//         }
+        
+//         int cnt = 0;
+//         while (head < n && progresses[head] >= 100) {
+//             head++;
+//             cnt++;
+//         }
+        
+//         if (cnt) answer.push_back(cnt);
+//     }
+    
+//     return answer;
+// }
+
+// 2026.09.21 - O(N) 풀이 (수식 보일 때)
 #include<bits/stdc++.h>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    int head = 0, n = (int) progresses.size();
     
-    while (head < n) {
-        for (int i = head; i < n; i++) {
-            progresses[i] += speeds[i];
-        }
+    int n = (int) progresses.size(), deployDay = 0;
+    for (int i = 0; i < n; i++) {
+        int day = (99 - progresses[i]) / speeds[i] + 1;
         
-        int cnt = 0;
-        while (head < n && progresses[head] >= 100) {
-            head++;
-            cnt++;
+        // 현재 배포일보다 큰 값이면 다른 그룹으로 묶고, 
+        if (answer.empty() || day > deployDay) {
+            deployDay = day;
+            answer.push_back(1);
+        // 현재 배포일보다 작거나 같은 값이면 같은 그룹으로 묶기
+        } else {
+            answer.back()++;
         }
-        
-        if (cnt) answer.push_back(cnt);
     }
     
     return answer;
