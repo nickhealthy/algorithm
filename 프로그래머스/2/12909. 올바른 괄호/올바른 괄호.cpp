9564dcb45d2576da8240e0fd9961e74f1a408d42
nick <alskadmlcraz1@gmail.com>
2026-09-21 22:15:21 +0900

    [level 2] Title: 올바른 괄호, Time: 0.36 ms, Memory: 4.58 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -4,18 +4,14 @@ using namespace std;
 
 
 bool solution(string s) {
-    stack<int> st;
-    
-    for (int i = 0; i < (int) s.length(); i++) {
-        char ch = s[i];
-        if (!st.empty() && ch == ')' && st.top() == '(') {
+    stack<char> st;
+    for (char ch: s) {
+        if (ch == '(') st.push(ch);
+        else {
+            if (st.empty()) return false;
             st.pop();
-        } else {
-            st.push(ch);
         }
     }
-    
-    if (st.empty()) return true;
 
-    return false;
+    return st.empty();
 }
