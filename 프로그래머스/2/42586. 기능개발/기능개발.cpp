d1708380149ea8b42ab6220fd2493975951c018f
nick <alskadmlcraz1@gmail.com>
2026-09-21 21:25:14 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,36 +1,61 @@
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// vector<int> solution(vector<int> progresses, vector<int> speeds) {
+//     vector<int> answer;
+//     queue<pair<int, int>> queue;
+    
+//     int n = (int)progresses.size();
+//     for (int i = 0; i < n; i++) {
+//         queue.push({progresses[i], speeds[i]});
+//     }
+    
+//     while (!queue.empty()) {
+//         int qSize = (int)queue.size();
+//         for (int i = 0; i < qSize; i++) {
+//             auto [progress, speed] = queue.front(); queue.pop();
+//             progress += speed;
+//             queue.push({progress, speed});
+//         }
+        
+//         if (queue.front().first >= 100) {
+//             int count = 0;
+//             //qSize = (int)queue.size();
+//             for (int i = 0; i < qSize; i++) {
+//                 if (queue.front().first >= 100) {
+//                     queue.pop();
+//                     count++;
+//                 }
+//             }
+            
+//             answer.push_back(count);
+//         }
+//     }
+    
+//     return answer;
+// }
+
 #include<bits/stdc++.h>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    queue<pair<int, int>> queue;
+    int head = 0, n = (int) progresses.size();
     
-    int n = (int)progresses.size();
-    for (int i = 0; i < n; i++) {
-        queue.push({progresses[i], speeds[i]});
-    }
-    
-    while (!queue.empty()) {
-        int qSize = (int)queue.size();
-        for (int i = 0; i < qSize; i++) {
-            auto [progress, speed] = queue.front(); queue.pop();
-            progress += speed;
-            queue.push({progress, speed});
+    while (head < n) {
+        for (int i = head; i < n; i++) {
+            progresses[i] += speeds[i];
         }
         
-        if (queue.front().first >= 100) {
-            int count = 0;
-            qSize = (int)queue.size();
-            for (int i = 0; i < qSize; i++) {
-                if (queue.front().first >= 100) {
-                    queue.pop();
-                    count++;
-                }
-            }
-            
-            answer.push_back(count);
+        int cnt = 0;
+        while (head < n && progresses[head] >= 100) {
+            head++;
+            cnt++;
         }
+        
+        if (cnt) answer.push_back(cnt);
     }
     
     return answer;
